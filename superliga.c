9810c393ce98be4a75f7cc59343d916f15f1d7a1
sipman <m.hartvigsen@gmail.com>
2016-11-24 12:56:02 +0100

    Now able to extract the amount of matches+rounds and save a line to a match struct - still need to make somehow to presists the matches

--- superliga.c
+++ superliga.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define SOURCEFILE "superliga-2015-2016"
 typedef struct {
   int day;
   int month;
@@ -19,13 +21,18 @@ typedef struct {
   int awayGoals;
   int attendances;
 } match;
-
+void initiateData(int *numberOfRounds, int *numberOfMatches, FILE *inputFile);
+match generateMatchFromStr(char *str);
+int getNumberOfRounds(char *round, int currentRoundNumber);
 int main(void){
-  match matches[2] = {
-                        {"R1", "Fre", {17,7,2015}, {18,00}, "FCN", "SDR", 0, 2, 3211},
-                        {"R1", "Lor", {18,7,2015}, {17,30}, "FCK", "BIF", 7, 0, 43211}
-                      };
-  printf("%s %s %d/%d/%d %d:%d %s - %s %d-%d %d\n",
+  int numberOfRounds=0, numberOfMatches=0;
+  FILE *input = fopen(SOURCEFILE, "r");
+  if (input == NULL){
+        exit(EXIT_FAILURE);
+  }
+  initiateData(&numberOfRounds, &numberOfMatches, input);
+
+  /*printf("%s %s %02d/%02d/%d %02d:%02d %s - %s %d-%d %d\n",
         matches[0].round,
         matches[0].weekDay,
         matches[0].date.day,
@@ -38,6 +45,49 @@ int main(void){
         matches[0].homeGoals,
         matches[0].awayGoals,
         matches[0].attendances
-    );
+    );*/
+  fclose(input);
+  printf("Number of matches: %d\n", numberOfMatches);
+  printf("Number of rounds: %d\n", numberOfRounds);
   return 0;
+}
+
+void initiateData(int *numberOfRounds, int *numberOfMatches, FILE *inputFile){
+  char str[255];
+  match currentMatch;
+   while (fgets(str, sizeof(str), inputFile)) {
+        *numberOfMatches += 1;
+        currentMatch = generateMatchFromStr(str);
+        *numberOfRounds = getNumberOfRounds(currentMatch.round, *numberOfRounds);
+    }
+}
+
+match generateMatchFromStr(char *str){
+  match currentMatch;
+  double attendances = 0;
+  sscanf(str, " %[R0123456789] %s %d/%d/%d %d.%d %s - %s %d - %d %lf",
+              currentMatch.round,
+              currentMatch.weekDay,
+              &currentMatch.date.day,
+              &currentMatch.date.month,
+              &currentMatch.date.year,
+              &currentMatch.time.hours,
+              &currentMatch.time.minutes,
+              currentMatch.homeTeam,
+              currentMatch.awayTeam,
+              &currentMatch.homeGoals,
+              &currentMatch.awayGoals,
+              &attendances
+        );
+  currentMatch.attendances = (attendances*1000);
+  return currentMatch;
+}
+
+int getNumberOfRounds(char *round, int currentRoundNumber){
+    int roundNum;
+    sscanf(round, "R%d", &roundNum);
+    if(roundNum < currentRoundNumber){
+        return currentRoundNumber;
+    }
+    return roundNum;
 }
